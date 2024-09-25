b298fd76f980e0ac8c022543859bf0f8b9cf3bab
mateusp7 <mateusdev1337@gmail.com>
2024-09-25 20:52:45 -0300

    feat: create user repository

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma";
+import { PrismaUsersRepository } from "@/repositories/prisma-users-repository";
 import { hash } from "bcryptjs";
 
 interface RegisterUseCaseRequest {
@@ -9,22 +10,17 @@ interface RegisterUseCaseRequest {
 
 export async function registerUseCase({ name, email, password }: RegisterUseCaseRequest) {
   const password_hash = await hash(password, 6);
+  const prismaUsersRepository = new PrismaUsersRepository()
 
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
+  const userWithSameEmail = await prismaUsersRepository.findByEmail(email);
 
   if (userWithSameEmail) {
     throw new Error("Email already exists");
   }
 
-  await prisma.user.create({
-    data: {
-      name: name,
-      email: email,
-      password_hash,
-    },
+  await prismaUsersRepository.create({
+    name,
+    email,
+    password_hash,
   });
 }
